9b4d2800ca80c39993970831447efd68810bfb85
Kris Artyukova <kristinaartyukova@yandex.ru>
2023-03-24 16:35:47 +0300

    version 1.5 (скрол)

---
+++ scripts/script.js
@@ -0,0 +1,53 @@
+    const editBtn = document.querySelector('.profile__btn-edit');
+    const closeBtn = document.querySelector('.popup__close-icon');
+    const saveBtn = document.querySelector('.form__btn');
+    const form = document.querySelector('.popup');
+    const profileTitle = document.querySelector('.profile__title');
+    const profileSubtitle = document.querySelector('.profile__subtitle');
+    let nameInput = document.querySelector('input.form__input-name');
+    let jobInput = document.querySelector('input.form__input-info');
+    // Находим форму в DOM
+    let formElement = document.querySelector('form');
+
+    function initial() {
+      nameInput.value = profileTitle.textContent;
+      jobInput.value = profileSubtitle.textContent;
+    }
+
+    function editBtnHandler() {
+      form.classList.add('form_opened');
+    }
+
+    function closeBtnHandler() {
+      form.classList.remove('form_opened');
+    }
+
+    function saveBtnHandler() {
+      form.classList.remove('form_opened');
+    }
+
+     // Обработчик «отправки» формы, хотя пока
+    // она никуда отправляться не будет
+    function formSubmitHandler (evt) {
+      evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+                            // Так мы можем определить свою логику отправки.
+                            // О том, как это делать, расскажем позже.
+
+      // Получите значение полей из свойства value
+      const nameInputValue = nameInput.value;
+      const jobInputValue = jobInput.value;
+
+      // Вставьте новые значения с помощью textContent
+      profileTitle.textContent = nameInputValue;
+      profileSubtitle.textContent = jobInputValue;
+    }
+
+    editBtn.addEventListener('click', editBtnHandler);
+    closeBtn.addEventListener('click', closeBtnHandler);
+    saveBtn.addEventListener('click', saveBtnHandler);
+
+    // Прикрепляем обработчик к форме:
+    // он будет следить за событием “submit” - «отправка»
+    formElement.addEventListener('submit', formSubmitHandler);
+
+    initial();
