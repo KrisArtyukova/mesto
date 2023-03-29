8b9847cc1e962d22518b45f35f0b987cdc614cb2
Kris Artyukova <kristinaartyukova@yandex.ru>
2023-03-29 20:55:25 +0300

    fix after the review 1

--- scripts/script.js
+++ scripts/script.js
@@ -4,8 +4,8 @@
     const popupEditProfile = document.querySelector('.popup');
     const profileTitle = document.querySelector('.profile__title');
     const profileSubtitle = document.querySelector('.profile__subtitle');
-    let nameInput = document.querySelector('input.form__input_type_name');
-    let jobInput = document.querySelector('input.form__input_type_info');
+    let nameInput = document.querySelector('.form__input_type_name');
+    let jobInput = document.querySelector('.form__input_type_info');
     // Находим форму в DOM
     let formElement = document.querySelector('form');
 
@@ -15,37 +15,29 @@
     }
 
     function editBtnHandler() {
-      popupEditProfile.classList.add('form_opened');
+      popupEditProfile.classList.add('popup_opened');
       initial();
     }
 
     function closeBtnHandler() {
-      popupEditProfile.classList.remove('form_opened');
+      popupEditProfile.classList.remove('popup_opened');
     }
 
-    function saveBtnHandler() {
-      popupEditProfile.classList.remove('form_opened');
-    }
-
-     // Обработчик «отправки» формы, хотя пока
+    // Обработчик «отправки» формы, хотя пока
     // она никуда отправляться не будет
     function formSubmitHandler (evt) {
       evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
                             // Так мы можем определить свою логику отправки.
                             // О том, как это делать, расскажем позже.
 
-      // Получите значение полей из свойства value
-      const nameInputValue = nameInput.value;
-      const jobInputValue = jobInput.value;
-
       // Вставьте новые значения с помощью textContent
-      profileTitle.textContent = nameInputValue;
-      profileSubtitle.textContent = jobInputValue;
+      profileTitle.textContent = nameInput.value;
+      profileSubtitle.textContent = jobInput.value;
+      popupEditProfile.classList.remove('popup_opened');
     }
 
     editBtn.addEventListener('click', editBtnHandler);
     closeBtn.addEventListener('click', closeBtnHandler);
-    saveBtn.addEventListener('click', saveBtnHandler);
 
     // Прикрепляем обработчик к форме:
     // он будет следить за событием “submit” - «отправка»
